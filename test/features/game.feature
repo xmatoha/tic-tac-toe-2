741a1c37a9edcef4e3bed42b91d860c32f12caec
Martin Hablak <martin.hablak@sk.ibm.com>
2022-04-13 14:41:52 +0200

    refactor: bdd tests - game initiation

--- test/features/game.feature
+++ test/features/game.feature
@@ -1,23 +1,13 @@
 Feature: Game rules
     Scenario: Game initiation
-        Given Board size 3
-         When User creates new game
-         Then User is assigned player name X
-          And New board of size 3 is created
-          And Board is empty
+        Given Two users willing to play
+         When User X creates new game
+         Then User X and O see empty board
+         And User X is first player to make a move
 
-    Scenario: Player change turns after each round
-        Given Current round user is X
-        When User makes move
-        Then Current round player changes to O
 
-    Scenario: Player occupies free cell after move
-        Given Some valid game state
-        And There is at least one empty cell
-        When User X makes a move to that cell
-        Then Cell should be occupied by user X
+    # Scenario: Player change turns after each round
+    #     Given Current X is user who suppose to make a move
+    #     When User X makes move
+    #     Then Next player who suppose to make a muve is user O
 
-    # Scenario: 
-    #     Given Some valid game state
-    #     When User makes a move to occupied cell
-    #     Then Game status should indicate invalid move
--- test/steps_definitions/game_steps.clj
+++ test/steps_definitions/game_steps.clj
@@ -1,59 +1,16 @@
 (ns steps-definitions.game-steps
   (:require [lambdaisland.cucumber.dsl :refer :all]
             [clojure.test :refer :all]
-            [tic-tac-toe-2.game :as sut]))
+            [tic-tac-toe-2.game :refer :all]))
 
-(defn parse-coords [string]
-  (map (fn [e] (- (Integer/parseInt e) 1)) (clojure.string/split string #",")))
+(Given "Two users willing to play" [state]
+       state)
 
-(Given "Board size {int}" [state int1]
-       (assoc state :board-size int1))
+(When "User X creates new game" [state]
+      (assoc state :game-state (new-game 3)))
 
-(When "User creates new game" [state]
-      (assoc state :game-state (sut/new-game 3)))
-
-(Then "User is assigned player name X" [state]
-      (is (= :x (get-in state [:game-state :next-player])))
-      state)
-
-(And "New board of size 3 is created" [state int1]
-     (is (= 9 (count (get-in state [:game-state :current-board]))))
-     state)
-
-(And "Board is empty" [state]
-     (is (= 9 (count (filter (fn [e] (= (:state e) :e)) (get-in state [:game-state :current-board]))))) state)
-
-(Given "Current round user is X" [state]
-       (assoc state :game-state (sut/new-game 3)))
-
-(When "User makes move" [state]
-      (assoc state :game-state (sut/game-round (get-in state [:game-state]))))
-
-(Then "Current round player changes to O" [state]
-      (is (= :o (get-in state [:game-state :next-player])))
-      state)
-
-(Given "Some valid game state" [state]
-       (assoc state :game-state (sut/new-game 3)))
-(Integer/parseInt "1")
-
-(And "There is at least one empty cell" [state]
-     (is (< 0 (count (filter (fn [e] (= :e (:state e))) (get-in state [:game-state :current-board]))))) state)
-
-(When "User X makes a move to that cell" [state]
-      (assoc-in
-       state
-       [:game-state :current-board]
-       (apply sut/occupy (get-in state [:game-state :current-board])
-              (concat
-               [1 1]
-               [:x]))))
-
-(Then "Cell should be occupied by user X" [state]
-      (let [coords [1 1]]
-        (is (= :x (:state
-                   (sut/board-elem-at
-                    (get-in state [:game-state :current-board])
-                    (first coords)
-                    (second coords)))))) state)
-
+(Then "User X and O see empty board" [state]
+      (is (= 9 (count (->> (get-in state [:game-state :current-board])
+                           (filter #(= :e (:state %))))))) state)
+(And "User X is first player to make a move" [state]
+     (is (= :x (get-in state [:game-state  :next-player]))) state)

0edc4ac95b97f20fa1651f3afed158b53b94e728
Martin Hablak <martin.hablak@sk.ibm.com>
2022-04-19 22:07:41 +0200

    test: added pact tests

--- project.clj
+++ project.clj
@@ -10,7 +10,8 @@
                  [cheshire "5.10.2"]
                  [lambdaisland/kaocha-cucumber "0.0-53"]
                  [lambdaisland/kaocha-cloverage "1.0.75"]
-                 [nubank/mockfn "0.7.0"]]
+                 [nubank/mockfn "0.7.0"]
+                 [au.com.dius.pact.consumer/junit "4.2.9"]]
   :main ^:skip-aot tic-tac-toe-2.core
   :target-path "target/%s"
   :aliases {"test" ["with-profile" "+kaocha" "run" "-m" "kaocha.runner" "unit"]
@@ -21,10 +22,20 @@
  ;           [cider/cider-nrepl "0.28.3"]
             [lein-bump-version "0.1.6"]]
   :jvm-opts ["-XX:TieredStopAtLevel=1", "-Xmx512m"]
-  :profiles {:kaocha {:dependencies [[lambdaisland/kaocha-cucumber "0.0-53"]
+  :pact {:service-providers {:test_provider {:protocol "http"
+                                             :host "localhost"
+                                             :port 3001
+                                             :path  "/"
+                                             :has-pact-with
+                                             {:clojure_test_consumer {:pact-source "file:./target/pacts/clojure_test_consumer-test_provider.json"}}}}}
+
+  :profiles {:pact {:plugins [[au.com.dius.pact.provider/lein "4.1.20"
+                               :exclusions [commons-logging]]] :dependencies
+                    [[ch.qos.logback/logback-core "1.2.3"]
+                     [ch.qos.logback/logback-classic "1.2.3"]]}
+             :kaocha {:dependencies [[lambdaisland/kaocha-cucumber "0.0-53"]
                                      [lambdaisland/kaocha "1.64.1010"]
                                      [lambdaisland/kaocha-cloverage "1.0.75"]]}
              :uberjar {:aot :all}})
-
 
 
---
+++ test/tic_tac_toe_2/api_test.clj
@@ -0,0 +1,31 @@
+(ns tic-tac-toe-2.api-test
+  (:require
+   [clojure.test :refer [deftest is]]
+   [clj-http.client :as http-client]
+   [cheshire.core :refer [generate-string]])
+
+  (:import [au.com.dius.pact.consumer
+            ConsumerPactBuilder ConsumerPactRunnerKt PactTestRun PactVerificationResult$Ok]
+           [au.com.dius.pact.consumer.model MockProviderConfig]))
+
+(deftest example-clojure-consumer-pact-test
+  (let [consumer-pact (-> "clojure_test_consumer"
+                          ConsumerPactBuilder/consumer
+                          (.hasPactWith "test_provider")
+                          (.uponReceiving "clojure test interaction")
+                          (.path "/health")
+                          (.method "GET")
+                          .willRespondWith
+                          (.status 200)
+                          (.body (generate-string {:msg "ok"}))
+                          .toPact)
+        config (-> (MockProviderConfig/createDefault))]
+    (is (instance? PactVerificationResult$Ok
+                   (ConsumerPactRunnerKt/runConsumerTest
+                    consumer-pact config
+                    (proxy [PactTestRun] []
+                      (run [mock-server _]
+                        (#(is (= (generate-string {:msg "ok"})
+                                 (:body
+                                  (http-client/get
+                                   (str (.getUrl mock-server) "/health")))))))))))))

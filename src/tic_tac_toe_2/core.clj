da7850ddf6ae933cf2f39d56840e95acf7b7cd9d
Martin Hablak <martin.hablak@sk.ibm.com>
2022-04-19 16:24:44 +0200

    fix: server main method

--- src/tic_tac_toe_2/core.clj
+++ src/tic_tac_toe_2/core.clj
@@ -2,5 +2,5 @@
   (:require [tic-tac-toe-2.server :refer [server-start]])
   (:gen-class))
 
-(defn main [& args]
+(defn -main [& args]
   (server-start (merge  (into  {} (System/getenv)) {"PORT" "3001"})))
